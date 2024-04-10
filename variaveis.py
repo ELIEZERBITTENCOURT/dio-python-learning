e5677967ec2c61d925a8d5e651e1107541a631fb
Eliezer Bittencourt <likobittencourt@gmail.com>
2024-04-10 14:56:28 -0300

    fix: var types

---
+++ variaveis.py
@@ -0,0 +1,20 @@
+a = 10
+print(a)
+# a variavel a se torna uma variável do tipo inteiro.
+b= 1.2
+print(b)
+# a variavel b se torna uma variável do tipo float.
+c = "olá  Mundo"
+print(c)
+# a variável c se torna uma variável do tipo string.
+
+# atribuição de valor para variàvel pode ser feita utilizando o comando 'input()'.
+nome = input("Entre com o seu nome: ")
+print(nome)
+# o comando 'input()' sempre retornará um a string. 
+# caso queira retornar dados do tipo inteiro ou float, é preciso converter o tipo:
+num = int(input("Entre com um número: "))
+print(num)
+
+altura = float(input("Entre com a sua altura: "))
+print(altura)
